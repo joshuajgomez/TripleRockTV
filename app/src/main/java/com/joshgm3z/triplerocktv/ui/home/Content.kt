5adfe081381e273f8dcabff7e477685951094788
joshua <joshuajgomez@gmail.com>
2026-01-20 16:55:30 +0100

    Home screen content placeholder styling

--- app/src/main/java/com/joshgm3z/triplerocktv/ui/home/Content.kt
+++ app/src/main/java/com/joshgm3z/triplerocktv/ui/home/Content.kt
@@ -1,12 +1,25 @@
 package com.joshgm3z.triplerocktv.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Text
+import com.joshgm3z.triplerocktv.R
+import com.joshgm3z.triplerocktv.ui.common.TvPreview
 import com.joshgm3z.triplerocktv.ui.theme.TripleRockTVTheme
 
 @Composable
@@ -17,6 +30,8 @@ fun Content() {
             .padding(top = 30.dp, start = 10.dp, end = 10.dp),
         maxItemsInEachRow = 5,
     ) {
+//        ContentPlaceholder()
+//        return@FlowRow
         ThumbnailCard()
         ThumbnailCard()
         ThumbnailCard()
@@ -28,7 +43,33 @@ fun Content() {
     }
 }
 
-@Preview
+@TvPreview
+@Composable
+fun ContentPlaceholder(text: String = "Fetching movies") {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .width(700.dp)
+            .height(400.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.movie),
+            contentDescription = null,
+            modifier = Modifier.size(100.dp)
+        )
+        Spacer(Modifier.size(10.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CircularProgressIndicator(modifier = Modifier.size(20.dp))
+            Spacer(Modifier.size(10.dp))
+            Text(text)
+        }
+    }
+}
+
+@TvPreview
 @Composable
 private fun PreviewContent() {
     TripleRockTVTheme {
