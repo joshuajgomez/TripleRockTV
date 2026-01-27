16b62f4987cbffece59fff25ca89efab7a7376d3
Joshua Gomez <joshua.gomez@volvocars.com>
2026-01-27 12:06:57 +0100

    change to tv Text

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/search/SearchScreen.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/search/SearchScreen.kt
@@ -8,10 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Text
 import com.joshgm3z.triplerocktv.repository.room.StreamEntity
 import com.joshgm3z.triplerocktv.ui.common.BackButton
 import com.joshgm3z.triplerocktv.ui.common.TvPreview
