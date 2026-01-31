51e26de3c8b8c6cbf4ddca819fe5f64c9826f8b8
joshua <joshuajgomez@gmail.com>
2026-01-31 22:06:53 +0100

    Add BrowseSupportFragment 3

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/LegacyMainActivity.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/LegacyMainActivity.kt
@@ -1,39 +1,17 @@
 package com.joshgm3z.triplerocktv
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.RecyclerView
-import com.joshgm3z.triplerocktv.ui.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LegacyMainActivity : FragmentActivity() {
 
-//    private val viewModel: HomeViewModel by viewModels()
-//    private val adapter = StreamAdapter()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Removed enableEdgeToEdge() as it can interfere with Leanback's layout and overscan handling.
         setContentView(R.layout.activity_legacy_main)
-
-        /*val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = adapter
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.uiState.collectLatest { uiState ->
-                    adapter.submitList(uiState.streamEntities)
-                }
-            }
-        }*/
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
