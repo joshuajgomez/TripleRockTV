d50d78abc5b82b6b84f01bfc9f8d4a2b482a10b4
joshua <joshuajgomez@gmail.com>
2026-02-06 07:20:14 +0100

    cleanup - bugfix

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/details/VodDetailsDescriptionPresenter.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/details/VodDetailsDescriptionPresenter.kt
@@ -3,10 +3,10 @@ package com.joshgm3z.triplerocktv.ui.details
 import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
 import com.joshgm3z.triplerocktv.repository.room.vod.VodStream
 
-class VodDetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
     override fun onBindDescription(vh: ViewHolder, item: Any) {
-        val stream = item as VodStream
-        vh.title.text = stream.name
+        val uiState = item as DetailsUiState
+        vh.title.text = uiState.title
 //        vh.subtitle.text = "Stream ID: ${stream.streamId}"
 //        vh.body.text = "Category ID: ${stream.categoryId}\nAdded: ${stream.added}"
     }
