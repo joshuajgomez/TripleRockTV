e00917e9642051063ae201570b2a8cdadd08bce8
Joshua Gomez <joshua.gomez@volvocars.com>
2026-02-09 12:56:25 +0100

    Add clicked subtitle to default list

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/player/subtitle/SubtitleDownloaderViewModel.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/player/subtitle/SubtitleDownloaderViewModel.kt
@@ -7,8 +7,14 @@ import com.joshgm3z.triplerocktv.repository.SubtitleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class SubtitleUiState(
+    val downloadedSubtitleList: List<SubtitleData>? = null,
+    val defaultSubtitleList: List<SubtitleData>? = null
+)
 
 @HiltViewModel
 class SubtitleDownloaderViewModel
@@ -17,18 +23,31 @@ constructor(
     private val subtitleRepository: SubtitleRepository
 ) : ViewModel() {
 
-    private val _subtitleList = MutableStateFlow<List<SubtitleData>?>(null)
-    val subtitleList = _subtitleList.asStateFlow()
+    private val _subtitleUiState = MutableStateFlow(SubtitleUiState())
+    val subtitleUiState = _subtitleUiState.asStateFlow()
 
     fun onFindClicked(query: String) {
         viewModelScope.launch {
-            _subtitleList.value = subtitleRepository.findSubtitles(query)
+            _subtitleUiState.update {
+                it.copy(downloadedSubtitleList = subtitleRepository.findSubtitles(query))
+            }
         }
     }
 
     fun onSubtitleClicked(subtitleData: SubtitleData) {
         viewModelScope.launch {
             subtitleRepository.storeSubtitle(subtitleData)
+            _subtitleUiState.update {
+                val list = it.defaultSubtitleList?.toMutableList() ?: mutableListOf()
+                it.copy(
+                    defaultSubtitleList = list.apply { add(subtitleData) })
+            }
+        }
+    }
+
+    fun onDefaultSubtitlesFound(list: List<SubtitleData>) {
+        _subtitleUiState.update {
+            it.copy(defaultSubtitleList = list)
         }
     }
 }
