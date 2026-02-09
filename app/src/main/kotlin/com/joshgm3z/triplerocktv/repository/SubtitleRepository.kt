6b8f72e60632e00b8bc9f25d1491b4c2ac3e2cdf
joshua <joshuajgomez@gmail.com>
2026-02-09 21:39:39 +0100

    set to default subtitle

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/repository/SubtitleRepository.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/repository/SubtitleRepository.kt
@@ -2,6 +2,7 @@ package com.joshgm3z.triplerocktv.repository
 
 data class SubtitleData(
     val title: String,
+    val language: String? = null,
     val fileId: Int,
     val url: String? = null,
 )
