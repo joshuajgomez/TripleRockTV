8d4f503d96028e1921c929e448dcab67f1e5821c
joshua <joshuajgomez@gmail.com>
2026-02-10 22:59:24 +0100

    language indicator

--- app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoSubtitleRepositoryImpl.kt
+++ app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoSubtitleRepositoryImpl.kt
@@ -11,19 +11,19 @@ constructor() : SubtitleRepository {
     override suspend fun findSubtitles(query: String): List<SubtitleData> {
         return listOf(
             SubtitleData(
-                title = "English",
+                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/english.srt",
             ),
             SubtitleData(
-                title = "Spanish",
+                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/spanish.srt",
             ),
             SubtitleData(
-                title = "French",
+                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/french.srt",
             ),
             SubtitleData(
-                title = "German",
+                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/german.srt",
             ),
         )
