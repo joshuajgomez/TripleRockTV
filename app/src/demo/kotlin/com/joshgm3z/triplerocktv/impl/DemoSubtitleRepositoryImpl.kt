e1a83ffc92ab0a2e741592e7790664360baccb3a
joshua <joshuajgomez@gmail.com>
2026-02-10 23:13:06 +0100

    update demo

--- app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoSubtitleRepositoryImpl.kt
+++ app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoSubtitleRepositoryImpl.kt
@@ -13,23 +13,27 @@ constructor() : SubtitleRepository {
             SubtitleData(
                 title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/english.srt",
-            ),
+                language = "en"
+                ),
             SubtitleData(
-                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
+                title = "The.Equalizer.2.2018.HYBRID.2160p.BluRay.REMUX.HEVC.DV.TrueHD.Atmos.7.1-Flights",
                 url = "https://example.com/spanish.srt",
+                language = "fr"
             ),
             SubtitleData(
-                title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
+                title = "The.Equalizer.2.2018.HYBRID.2160p.BluRay.REMUX.HEVC.DV.TrueHD.Atmos.7.1-Flights",
                 url = "https://example.com/french.srt",
+                language = "ro"
             ),
             SubtitleData(
                 title = "Wonder.Woman.1984.2020.720p.WEB.H264-NAISU.ur",
                 url = "https://example.com/german.srt",
+                language = "fi"
             ),
         )
     }
 
-    override suspend fun storeSubtitle(subtitleData: SubtitleData) {
-        delay(1000)
+    override suspend fun getSubtitleUrl(fileId: Int): String? {
+        return ""
     }
 }
