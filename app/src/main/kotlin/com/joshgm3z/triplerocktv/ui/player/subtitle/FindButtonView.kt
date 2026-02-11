5ea70fb1676b12a6ffc5befdb186e070918494ad
joshua <joshuajgomez@gmail.com>
2026-02-11 22:31:26 +0100

    beautify 2

---
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/player/subtitle/FindButtonView.kt
@@ -0,0 +1,17 @@
+package com.joshgm3z.triplerocktv.ui.player.subtitle
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.joshgm3z.triplerocktv.databinding.ViewFindButtonBinding
+
+class FindButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+    init {
+        ViewFindButtonBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+}
