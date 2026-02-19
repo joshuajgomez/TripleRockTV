5adc172ff9a9078b4443aa80cc6f26043e852fcd
joshua <joshuajgomez@gmail.com>
2026-02-19 21:32:06 +0100

    fix build error

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/details/StreamDataDetailsDescriptionPresenter.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/details/StreamDataDetailsDescriptionPresenter.kt
@@ -1,7 +1,6 @@
 package com.joshgm3z.triplerocktv.ui.details
 
 import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
-import com.joshgm3z.triplerocktv.repository.room.vod.VodStream
 
 class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
     override fun onBindDescription(vh: ViewHolder, item: Any) {
