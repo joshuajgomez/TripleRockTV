385df9d598b7673715c52667b460624cbc316eb6
Joshua Gomez <joshua.gomez@volvocars.com>
2026-02-25 15:56:47 +0100

    Fix build errors in demo

--- app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoMediaOnlineRepositoryImpl.kt
+++ app/src/demo/kotlin/com/joshgm3z/triplerocktv/impl/DemoMediaOnlineRepositoryImpl.kt
@@ -2,8 +2,8 @@ package com.joshgm3z.triplerocktv.impl
 
 import com.joshgm3z.triplerocktv.repository.LoadingState
 import com.joshgm3z.triplerocktv.repository.LoadingStatus
-import com.joshgm3z.triplerocktv.repository.MediaLoadingType
 import com.joshgm3z.triplerocktv.repository.MediaOnlineRepository
+import com.joshgm3z.triplerocktv.repository.StreamType
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
@@ -11,10 +11,10 @@ class DemoMediaOnlineRepositoryImpl
 @Inject
 constructor() : MediaOnlineRepository {
     override suspend fun fetchContent(
-        onFetch: (MediaLoadingType, LoadingState) -> Unit,
+        onFetch: (StreamType, LoadingState) -> Unit,
         onError: (String, String) -> Unit
     ) {
-        MediaLoadingType.entries.forEach { type ->
+        StreamType.entries.forEach { type ->
             repeat(10) { i ->
                 onFetch(type, LoadingState(i * 10, LoadingStatus.Ongoing))
                 delay(200)
