12a7727297498567f6e772c92b0cac2885e37e9f
Joshua Gomez <joshua.gomez@volvocars.com>
2026-03-13 08:55:20 +0100

    show duration in details fragment

---
+++ app/src/test/kotlin/com/joshgm3z/triplerocktv/repository/room/ToTextTimeTest.kt
@@ -0,0 +1,14 @@
+package com.joshgm3z.triplerocktv.repository.room
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ToTextTimeTest {
+
+    @Test
+    fun test() {
+        assertEquals("1 hour 23 minutes", 4980000L.toTextTime())
+        assertEquals("", 0L.toTextTime())
+        assertEquals("45 minutes", 2700000L.toTextTime())
+    }
+}
