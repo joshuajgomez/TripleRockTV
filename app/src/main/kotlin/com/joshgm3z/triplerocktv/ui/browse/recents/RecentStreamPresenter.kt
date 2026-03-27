c5589ca6898cdebe9a198865a71da34a28bc20f6
joshua <joshuajgomez@gmail.com>
2026-03-27 21:38:28 +0100

    recents fix

--- app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/browse/recents/RecentStreamPresenter.kt
+++ app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/browse/recents/RecentStreamPresenter.kt
@@ -9,6 +9,7 @@ import androidx.leanback.widget.Presenter
 import com.bumptech.glide.Glide
 import com.joshgm3z.triplerocktv.R
 import com.joshgm3z.triplerocktv.databinding.ViewRecentStreamCardBinding
+import com.joshgm3z.triplerocktv.repository.data.Episode
 import com.joshgm3z.triplerocktv.repository.room.StreamData
 import com.joshgm3z.triplerocktv.repository.room.series.SeriesStream
 import com.joshgm3z.triplerocktv.util.GlideUtil
@@ -32,26 +33,25 @@ class RecentStreamPresenter
     override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
         val title = when (item) {
             is StreamData -> item.name
-            is SeriesStream -> item.name
+            is Episode -> item.title
             else -> "Unknown"
         }
         val imageUri = when (item) {
             is StreamData -> item.streamIcon
-            is SeriesStream -> item.coverImageUrl
+            is Episode -> item.episodeInfo?.movie_image
             else -> "Unknown"
         }
         val progress = when (item) {
             is StreamData -> item.progressPercent()
+            is Episode -> item.progressPercent()
             else -> 0
         }
-        val titleView = viewHolder.view.findViewById<TextView>(R.id.stream_title)
-        val imageView = viewHolder.view.findViewById<ImageView>(R.id.poster_image)
-        val progressBar = viewHolder.view.findViewById<ProgressBar>(R.id.progress_bar)
-
-        titleView.text = title
-        glideUtil.loadImage(imageUri, imageView)
-        progressBar.progress = progress
-        progressBar.visibility = if (progress > 0) ProgressBar.VISIBLE else ProgressBar.GONE
+        ViewRecentStreamCardBinding.bind(viewHolder.view).apply {
+            streamTitle.text = title
+            glideUtil.loadImage(imageUri, posterImage)
+            progressBar.progress = progress
+            progressBar.visibility = if (progress > 0) ProgressBar.VISIBLE else ProgressBar.GONE
+        }
     }
 
     override fun onUnbindViewHolder(viewHolder: ViewHolder) {
