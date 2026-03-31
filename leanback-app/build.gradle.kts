7594730cee1bda0847c1d3f0da942bf35a386e26
Joshua Gomez <joshua.gomez@volvocars.com>
2026-03-31 11:58:07 +0200

    remove unnecessary dependencies

--- leanback-app/build.gradle.kts
+++ leanback-app/build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.navigation.safeargs)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
     kotlin("kapt")
-    id("androidx.navigation.safeargs")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -87,36 +87,18 @@ android {
 dependencies {
     api(project(":core"))
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.tv.foundation)
-    implementation(libs.androidx.tv.material)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.datastore.core)
-    implementation(libs.androidx.datastore.preferences)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.leanback)
     implementation(libs.androidx.fragment.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
+
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.hilt.navigation.fragment)
 
     implementation(libs.hilt.android)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.annotation)
-    implementation(libs.google.material)
-    implementation(libs.androidx.leanback.preference)
     kapt(libs.hilt.compiler)
-
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.gson)
-
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    kapt(libs.room.compiler)
 
     implementation(libs.glide)
 
@@ -129,14 +111,14 @@ dependencies {
     implementation(libs.androidx.media3.leanback)
     implementation(libs.androidx.media3.hls)
     implementation(libs.glide.transformations)
-    implementation(libs.androidx.hilt.navigation.fragment)
+
     testImplementation(kotlin("test"))
 
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockk)
 
-    testImplementation("androidx.navigation:navigation-testing:2.8.5")
-    testImplementation("org.robolectric:robolectric:4.11.1")
-    testImplementation("androidx.test.ext:junit:1.2.1")
-    testImplementation("com.google.truth:truth:1.1.5")
+    testImplementation(libs.androidx.navigation.testing)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.androidx.junit)
+    testImplementation(libs.truth)
 }
