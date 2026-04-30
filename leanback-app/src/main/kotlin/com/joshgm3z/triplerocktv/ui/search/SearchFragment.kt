7601afbcc16d956f7153e1d85928e63e10373e67
joshua <joshuajgomez@gmail.com>
2026-04-30 20:09:05 +0200

    use new searchfragment

--- leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/search/SearchFragment.kt
+++ leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/search/SearchFragment.kt
@@ -1,170 +1,124 @@
 package com.joshgm3z.triplerocktv.ui.search
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.activityViewModels
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.leanback.app.SearchSupportFragment
-import androidx.leanback.widget.ArrayObjectAdapter
-import androidx.leanback.widget.HeaderItem
-import androidx.leanback.widget.ListRow
-import androidx.leanback.widget.ListRowPresenter
-import androidx.leanback.widget.ObjectAdapter
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import com.joshgm3z.triplerocktv.R
 import com.joshgm3z.triplerocktv.core.repository.StreamType
 import com.joshgm3z.triplerocktv.core.repository.room.StreamData
 import com.joshgm3z.triplerocktv.core.repository.room.series.SeriesStream
-import com.joshgm3z.triplerocktv.ui.streamcatalogue.StreamPresenter
-import com.joshgm3z.triplerocktv.core.util.Logger
-import com.joshgm3z.triplerocktv.core.viewmodel.OnlineTyperViewModel
 import com.joshgm3z.triplerocktv.core.viewmodel.SearchUiState
 import com.joshgm3z.triplerocktv.core.viewmodel.SearchViewModel
+import com.joshgm3z.triplerocktv.databinding.FragmentSearchBinding
+import com.joshgm3z.triplerocktv.ui.streamcatalogue.StreamPresenter
+import com.joshgm3z.triplerocktv.util.GlideUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.collections.isNotEmpty
 
 @AndroidEntryPoint
-class SearchFragment : SearchSupportFragment(), SearchSupportFragment.SearchResultProvider {
+class SearchFragment : Fragment() {
 
-    private lateinit var rowsAdapter: ArrayObjectAdapter
+    private lateinit var binding: FragmentSearchBinding
 
     private val viewModel: SearchViewModel by viewModels()
-
-    private val onlineTyperViewModel: OnlineTyperViewModel by activityViewModels()
 
     @Inject
     lateinit var streamPresenter: StreamPresenter
 
+    @Inject
+    lateinit var glideUtil: GlideUtil
+
+    private lateinit var streamAdapter: StreamAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
-        setSearchResultProvider(this)
+        streamAdapter = StreamAdapter(glideUtil) { onSearchResultClick(it) }
+    }
+
+    private fun onSearchResultClick(it: Any) {
+        val action = SearchFragmentDirections.toDetails()
+        var text = ""
+        when (it) {
+            is StreamData -> action.apply {
+                text = it.name
+                streamId = it.streamId
+                streamType = it.streamType
+            }
+
+            is SeriesStream -> action.apply {
+                text = it.name
+                streamId = it.seriesId
+                streamType = StreamType.Series
+            }
+
+            else -> return
+        }.let { action ->
+            viewModel.saveSearchHint(text)
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Set a title for the search fragment
-        title = "Movies, Series or Live TV"
+        initViews()
         lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.uiState.collectLatest {
-                    when (it.searchUiState) {
-                        is SearchUiState.Initial -> showInitialUi(it.searchHints)
-                        is SearchUiState.Loading -> showLoadingUi()
-                        is SearchUiState.Result -> updateSearchResult(it.searchUiState as SearchUiState.Result)
-                    }
+            viewModel.uiState.collectLatest {
+                binding.rvHints.adapter = SimpleTextAdapter(it.searchHints) { text ->
+                    binding.keyboardView.text = text
+                }
+                when (it.searchUiState) {
+                    is SearchUiState.Initial -> showInitialUi()
+
+                    is SearchUiState.Loading -> showLoadingUi()
+
+                    is SearchUiState.Result -> updateSearchResult(it.searchUiState as SearchUiState.Result)
                 }
             }
         }
-        lifecycleScope.launch {
-            onlineTyperViewModel.inputTextFlow.collectLatest {
-                setSearchQuery(it, false)
-            }
-        }
-        setupClickListeners()
     }
-
-    private fun setupClickListeners() = setOnItemViewClickedListener { _, item, _, _ ->
-        val action = SearchFragmentDirections.toDetails()
-        var itemName = ""
-        when (item) {
-            is StreamData -> action.apply {
-                itemName = item.name
-                streamId = item.streamId
-                streamType = item.streamType
-            }
-
-            is SeriesStream -> action.apply {
-                itemName = item.name
-                streamId = item.seriesId
-                streamType = StreamType.Series
-            }
-
-            is String -> {
-                setSearchQuery(item, true)
-                return@setOnItemViewClickedListener
-            }
-
-            else -> return@setOnItemViewClickedListener
-        }.let {
-            viewModel.saveSearchHint(itemName)
-            findNavController().navigate(it)
-        }
-    }
-
 
     private fun showLoadingUi() {
-        rowsAdapter.clear()
-        val header = HeaderItem(0, "Searching")
-        rowsAdapter.add(ListRow(header, ArrayObjectAdapter()))
+        streamAdapter.items = emptyList()
+        binding.tvStatus.text = "Searching"
     }
 
-    private fun showInitialUi(searchHints: List<String>) {
-        Logger.debug("searchHints = [${searchHints}]")
-        rowsAdapter.clear()
-        val header = HeaderItem(0, "Start typing to search")
-        val adapter = ArrayObjectAdapter(SimpleTextPresenter(R.drawable.ic_history))
-        adapter.addAll(0, searchHints)
-        rowsAdapter.add(ListRow(header, adapter))
+    private fun showInitialUi() {
+        streamAdapter.items = emptyList()
+        binding.tvStatus.text = ""
+        binding.rvSearchList.isFocusable = false
+    }
+
+    private fun initViews() {
+        binding.keyboardView.listener = object : KeyboardViewListener {
+            override fun onTextChange(text: String) {
+                binding.etInput.setText(text)
+                viewModel.onSearchInputChange(text)
+            }
+        }
+        binding.rvSearchList.adapter = streamAdapter
     }
 
     private fun updateSearchResult(result: SearchUiState.Result) {
-        Logger.debug("result = [${result}]")
-        rowsAdapter.clear()
-        when {
-            result.isEmpty() -> {
-                val header = HeaderItem(0, "No results found for '${result.query}'")
-                rowsAdapter.add(ListRow(header, ArrayObjectAdapter()))
-            }
-
-            else -> {
-                val vodStreamDataList = result.streamDataList.filter {
-                    it.streamType == StreamType.VideoOnDemand
-                }
-                if (vodStreamDataList.isNotEmpty()) addResults(
-                    "Video on demand",
-                    vodStreamDataList
-                )
-                val liveTvStreamDataList = result.streamDataList.filter {
-                    it.streamType == StreamType.LiveTV
-                }
-                if (liveTvStreamDataList.isNotEmpty()) addResults(
-                    "Live TV", liveTvStreamDataList
-                )
-                if (result.seriesStreams.isNotEmpty()) addResults("Series", result.seriesStreams)
-            }
+        val results = result.streamDataList + result.seriesStreams
+        binding.tvStatus.text = when {
+            results.isEmpty() -> "No results found"
+            else -> ""
         }
-    }
-
-    private fun addResults(headerText: String, result: List<Any>) {
-        val header = HeaderItem(0, "$headerText (${result.size})")
-        val adapter = ArrayObjectAdapter(streamPresenter)
-        adapter.addAll(0, result)
-        rowsAdapter.add(ListRow(header, adapter))
-    }
-
-    override fun getResultsAdapter(): ObjectAdapter {
-        return rowsAdapter
-    }
-
-    override fun onQueryTextChange(newQuery: String?): Boolean {
-        Logger.debug("newQuery = [${newQuery}]")
-        viewModel.onSearchInputChange(newQuery?.trim() ?: "")
-        return true
-    }
-
-    override fun onQueryTextSubmit(query: String?): Boolean {
-        Logger.debug("query = [${query}]")
-        query?.let {
-            viewModel.saveSearchHint(it)
-        }
-        return false
+        streamAdapter.items = results
     }
 }
