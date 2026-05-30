ad880d9086a41d38a369190e1b88e53510e03f4c
joshua <joshuajgomez@gmail.com>
2026-05-31 01:20:46 +0200

    Empty seasons check

--- core/src/main/kotlin/com/joshgm3z/triplerocktv/core/repository/impl/helper/SeriesFetcher.kt
+++ core/src/main/kotlin/com/joshgm3z/triplerocktv/core/repository/impl/helper/SeriesFetcher.kt
@@ -140,7 +140,7 @@ constructor(
                         voteAverage = seasonData.voteAverage ?: 0f,
                         overview = seasonData.overview ?: "",
                     )
-                } else listOf(
+                } else if (it.episodes.isNotEmpty()) listOf(
                     Season(
                         episodes = it.episodes[1] ?: emptyList(),
                         number = 1,
@@ -149,7 +149,7 @@ constructor(
                         voteAverage = 0f,
                         overview = "",
                     )
-                )
+                ) else emptyList()
                 Logger.debug("seasons = [$seasons]")
                 val filteredSeasons = seasons.filter { it.episodes.isNotEmpty() }
                 seriesStreamsDao.getBySeriesId(streamId).copy(seasons = filteredSeasons).let {
