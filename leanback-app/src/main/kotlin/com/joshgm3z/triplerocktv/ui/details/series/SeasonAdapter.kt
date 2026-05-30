5ff7588f5f0b19aa456431e94ee4fc148de3bd72
hellxboy <joshuajgomez@gmail.com>
2026-05-30 08:18:48 +0200

    Add new details screen for series

---
+++ leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/details/series/SeasonAdapter.kt
@@ -0,0 +1,58 @@
+package com.joshgm3z.triplerocktv.ui.details.series
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joshgm3z.triplerocktv.core.repository.room.series.Season
+import com.joshgm3z.triplerocktv.databinding.ItemTextChipBinding
+
+class SeasonAdapter(
+    private val onSeasonClick: (Season) -> Unit
+) : RecyclerView.Adapter<SeasonViewHolder>() {
+
+    var seasons: List<Season> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var selectedSeasonNumber: Int? = null
+        set(value) {
+            field?.let {
+                notifyItemChanged(it - 1)
+            }
+            field = value
+            value?.let {
+                notifyItemChanged(it - 1)
+            }
+        }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SeasonViewHolder {
+        val binding = ItemTextChipBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return SeasonViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(
+        holder: SeasonViewHolder,
+        position: Int
+    ) {
+        val binding = ItemTextChipBinding.bind(holder.itemView)
+        val season = seasons[position]
+        binding.tvText.text = "Season ${season.number}"
+        binding.root.setOnClickListener {
+            onSeasonClick(season)
+        }
+        binding.root.isSelected = selectedSeasonNumber == season.number
+    }
+
+    override fun getItemCount() = seasons.size
+}
+
+class SeasonViewHolder(view: View) : RecyclerView.ViewHolder(view)
