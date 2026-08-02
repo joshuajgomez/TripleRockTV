7019dba3cf261201d22a3a69cac24bb3eed05434
joshua <joshuajgomez@gmail.com>
2026-08-02 11:07:44 +0200

    Improve StreamDetailsScreen.kt

--- compose-app/src/main/java/com/joshgm3z/triplerocktv/compose/screens/common/CloseButton.kt
+++ compose-app/src/main/java/com/joshgm3z/triplerocktv/compose/screens/common/CloseButton.kt
@@ -1,5 +1,6 @@
 package com.joshgm3z.triplerocktv.compose.screens.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -11,18 +12,25 @@ import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CloseButton(
+    modifier: Modifier = Modifier,
     size: Dp = 30.dp,
+    showBackground: Boolean = false,
     onClick: () -> Unit = {},
 ) {
     IconButton(
         onClick = onClick,
-        modifier = Modifier
+        modifier = modifier
             .clip(CircleShape)
+            .background(
+                if (showBackground) colorScheme.background
+                else Color.Transparent
+            )
             .size(size)
     ) {
         Icon(
@@ -38,5 +46,13 @@ fun CloseButton(
 private fun PreviewCloseButton() {
     DarkSurface {
         CloseButton()
+    }
+}
+
+@DarkPreview
+@Composable
+private fun PreviewCloseButton_showBackground() {
+    DarkSurface {
+        CloseButton(showBackground = true)
     }
 }
