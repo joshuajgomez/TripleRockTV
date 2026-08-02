733d8134c05b8738ca89320ddee9d0ba6ba12dab
joshua <joshuajgomez@gmail.com>
2026-08-02 09:45:12 +0200

    Some changes

--- compose-app/src/main/java/com/joshgm3z/triplerocktv/compose/screens/settings/AppUpdateScreen.kt
+++ compose-app/src/main/java/com/joshgm3z/triplerocktv/compose/screens/settings/AppUpdateScreen.kt
@@ -1,25 +1,12 @@
 package com.joshgm3z.triplerocktv.compose.screens.settings
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.MaterialTheme.typography
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.joshgm3z.triplerocktv.compose.screens.common.ButtonItem
 import com.joshgm3z.triplerocktv.compose.screens.common.DarkPreview
-import com.joshgm3z.triplerocktv.compose.screens.common.PrimaryButton
-import com.joshgm3z.triplerocktv.compose.screens.common.SecondaryButton
-import com.joshgm3z.triplerocktv.compose.theme.TripleRockTvTheme
+import com.joshgm3z.triplerocktv.compose.screens.common.DarkSurface
+import com.joshgm3z.triplerocktv.compose.screens.common.InfoWithButtons
 import com.joshgm3z.triplerocktv.core.viewmodel.ButtonAction
 import com.joshgm3z.triplerocktv.core.viewmodel.SelfUpdateUiState
 import com.joshgm3z.triplerocktv.core.viewmodel.SelfUpdateViewModel
@@ -44,63 +31,48 @@ private fun AppUpdateScreenContent(
     onDismissClick: () -> Unit = {},
     onActionClick: () -> Unit = {},
 ) {
-    SettingScaffold(
+    InfoWithButtons(
         title = uiState.title,
-        onBackClick = onDismissClick
-    ) {
-        UpdateContent(
-            uiState = uiState,
-            onDismissClick = onDismissClick,
-            onActionClick = onActionClick
-        )
-    }
-}
-
-@Composable
-fun ColumnScope.UpdateContent(
-    uiState: SelfUpdateUiState,
-    onDismissClick: () -> Unit = {},
-    onActionClick: () -> Unit = {}
-) {
-    Text(
-        text = uiState.subtitle ?: "",
-        style = typography.bodyLarge,
-        color = colorScheme.onBackground,
-    )
-    AnimatedVisibility(
-        visible = !uiState.enableButtons,
-        modifier = Modifier.padding(vertical = 20.dp)
-    ) {
-        CircularProgressIndicator()
-    }
-    Spacer(
-        Modifier
-            .fillMaxSize()
-            .weight(1f)
-    )
-    PrimaryButton(
-        onClick = onActionClick,
-        enabled = uiState.enableButtons,
-        modifier = Modifier.fillMaxWidth(),
-        text = uiState.buttonAction.text
-    )
-    Spacer(Modifier.size(5.dp))
-    SecondaryButton(
-        onClick = onDismissClick,
-        enabled = uiState.enableButtons,
-        modifier = Modifier.fillMaxWidth(),
-        text = "Dismiss"
+        message = uiState.subtitle,
+        buttons = listOf(
+            ButtonItem(
+                primary = true,
+                loading = !uiState.enableButtons,
+                text = uiState.buttonAction.text,
+                onClick = onActionClick,
+                enabled = uiState.enableButtons
+            ),
+            ButtonItem(
+                text = "Dismiss",
+                onClick = onDismissClick,
+                enabled = uiState.enableButtons
+            )
+        ),
     )
 }
 
 @DarkPreview
 @Composable
-private fun PreviewAppUpdateScreen() {
-    TripleRockTvTheme {
+private fun PreviewAppUpdateScreen_UpdateNow() {
+    DarkSurface {
         AppUpdateScreenContent(
             SelfUpdateUiState(
                 title = "Update available",
                 subtitle = "New version 1.0.0 is available for download",
+                enableButtons = true,
+                buttonAction = ButtonAction.UpdateNow
+            )
+        )
+    }
+}
+
+@DarkPreview
+@Composable
+private fun PreviewAppUpdateScreen_CheckingUpdates() {
+    DarkSurface {
+        AppUpdateScreenContent(
+            SelfUpdateUiState(
+                title = "Checking updates",
                 enableButtons = false,
                 buttonAction = ButtonAction.UpdateNow
             )
