e92c71b23c0b74ed80fc9df5044c24becc835070
joshua <joshuajgomez@gmail.com>
2026-09-13 18:37:50 +0200

    Use ChannelPresenter for LiveTV BrowseFragment

--- leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/browse/CategoryPresenter.kt
+++ leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/browse/CategoryPresenter.kt
@@ -1,10 +1,15 @@
 package com.joshgm3z.triplerocktv.ui.browse
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.leanback.widget.Presenter
+import com.bumptech.glide.Glide
+import com.joshgm3z.triplerocktv.R
+import com.joshgm3z.triplerocktv.core.repository.StreamType
 import com.joshgm3z.triplerocktv.databinding.ViewCategoryCardBinding
 import com.joshgm3z.triplerocktv.core.repository.room.category.CategoryData
+import com.joshgm3z.triplerocktv.core.util.Logger
 import com.joshgm3z.triplerocktv.core.util.withComma
 import com.joshgm3z.triplerocktv.util.GlideUtil
 import javax.inject.Inject
@@ -20,7 +25,8 @@ class CategoryPresenter
         return ViewHolder(binding.root)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any?) {
         val title = when (item) {
             is CategoryData -> item.categoryName
             else -> "Unknown"
@@ -33,13 +39,38 @@ class CategoryPresenter
             is CategoryData -> item.count
             else -> 0
         }
+        val streamType = when (item) {
+            is CategoryData -> item.streamType
+            else -> null
+        }
         val binding = ViewCategoryCardBinding.bind(viewHolder.view)
         binding.tvTitle.text = title
         binding.tvCount.text = "${count.withComma()} videos"
-        glideUtil.loadImage(streamIcon, binding.ivPoster)
+
+        when (streamType) {
+            StreamType.LiveTV -> glideUtil.loadImage(
+                streamIcon,
+                binding.ivIcon,
+                error = R.drawable.ic_video_file,
+                onSuccess = {
+                    binding.ivIcon.imageTintList = null
+                }
+            )
+
+            else -> glideUtil.loadImage(
+                streamIcon,
+                binding.ivPoster
+            )
+        }
     }
 
     override fun onUnbindViewHolder(viewHolder: ViewHolder) {
-
+        val binding = ViewCategoryCardBinding.bind(viewHolder.view)
+        Glide.with(binding.root.context).clear(binding.ivPoster)
+        Glide.with(binding.root.context).clear(binding.ivIcon)
+        binding.ivIcon.imageTintList = binding.root.context.getColorStateList(
+            com.joshgm3z.triplerocktv.core.R.color.color_foreground_high_selector
+        )
+        binding.ivIcon.setImageResource(R.drawable.ic_video_file)
     }
 }
