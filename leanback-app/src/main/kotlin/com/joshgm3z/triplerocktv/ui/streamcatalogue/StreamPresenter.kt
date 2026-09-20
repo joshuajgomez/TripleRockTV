b1caf74d7c99b1e9aaafa0177e2568bfc6faf3fd
joshua <joshuajgomez@gmail.com>
2026-09-20 09:08:42 +0200

    Beautify

--- leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/streamcatalogue/StreamPresenter.kt
+++ leanback-app/src/main/kotlin/com/joshgm3z/triplerocktv/ui/streamcatalogue/StreamPresenter.kt
@@ -3,13 +3,11 @@ package com.joshgm3z.triplerocktv.ui.streamcatalogue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.leanback.widget.Presenter
 import com.bumptech.glide.Glide
 import com.joshgm3z.triplerocktv.R
 import com.joshgm3z.triplerocktv.core.repository.StreamType
 import com.joshgm3z.triplerocktv.databinding.ViewStreamCardBinding
-import com.joshgm3z.triplerocktv.core.repository.impl.helper.parseToFloat
 import com.joshgm3z.triplerocktv.core.repository.room.stream.StreamData
 import com.joshgm3z.triplerocktv.core.repository.room.series.SeriesStream
 import com.joshgm3z.triplerocktv.util.setVisible
@@ -42,19 +40,11 @@ class StreamPresenter
             is SeriesStream -> item.coverImageUrl
             else -> null
         }
-        val rating = when (item) {
-            is StreamData -> item.rating
-            is SeriesStream -> item.rating.parseToFloat()
-            else -> null
-        }
         val streamType = when (item) {
             is StreamData -> item.streamType
             is SeriesStream -> StreamType.Series
             else -> null
         }
-
-        binding.tvRating.text = rating.toString()
-        binding.tvRating.setVisible(rating != null && rating > 0)
 
         binding.streamTitle.text = title
 
